521addfc11219dcd2a5dafe92621e23bcb040c10
Yashesh Mehta <yasheshmehta2005@gmail.com>
2025-11-15 11:31:22 +0530

    Day 12 (15/11): NULL Values and IS NULL/IS NOT NULL

--- Day-12/Day 12 NULL Values and IS NULL OR IS NOT NULL.sql
+++ Day-12/Day 12 NULL Values and IS NULL OR IS NOT NULL.sql
@@ -57,10 +57,13 @@ WHERE event IS NOT NULL AND event != '';
 
 -- Practice Questions:
 -- 1. Find all weeks in services_weekly where no special event occurred.
-SELECT * FROM services_weekly WHERE event IS NULL OR TRIM(event) = 'none' OR LOWER(event) = 'none';
+SELECT * FROM services_weekly 
+WHERE event IS NULL OR TRIM(event) = 'none' OR LOWER(event) = 'none';
 
 -- 2. Count how many records have null or empty event values.
-SELECT COUNT(*) AS total_null_records FROM services_weekly WHERE event IS NULL OR event = 'none';
+SELECT COUNT(*) AS total_null_records 
+FROM services_weekly 
+WHERE event IS NULL OR LOWER(event) = 'none' OR TRIM(event) = 'none';
 
 -- 3. List all services that had at least one week with a special event.
 SELECT DISTINCT service FROM services_weekly WHERE event IS NOT NULL AND TRIM(event) <> '' AND LOWER(event) <> 'none';
