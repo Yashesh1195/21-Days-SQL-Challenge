9f3f142f87ea7817fa13b1605dcacc38487f8233
Yashesh Mehta <yasheshmehta2005@gmail.com>
2025-11-22 07:52:47 +0530

    Day 7 (10/11): HAVING Clause

--- Day-7/Day 7 HAVING Clause.sql
+++ Day-7/Day 7 HAVING Clause.sql
@@ -83,10 +83,10 @@ HAVING total_staff_present < 50;
 -- Question: Identify services that refused more than 100 patients in total and 
 -- had an average patient satisfaction below 80. 
 -- Show service name, total refused, and average satisfaction.
-SELECT
+SELECT 
 	service,
     SUM(patients_refused) AS total_patients_refused,
-    AVG(patient_satisfaction) AS avg_sat
+    ROUND(AVG(patient_satisfaction), 2) AS avg_satisfaction
 FROM services_weekly
 GROUP BY service
-HAVING total_patients_refused > 100 AND avg_sat < 80;
+HAVING total_patients_refused > 100 AND avg_satisfaction < 80;
