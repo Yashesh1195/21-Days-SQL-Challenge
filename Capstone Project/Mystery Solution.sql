59403a8e0dd1c085daf17300bcd710ce4520b591
Yashesh Mehta <yasheshmehta2005@gmail.com>
2025-11-28 07:07:46 +0530

    Capstone Project - SQL Murder Mystery: “Who Killed the CEO?”

--- Capstone Project/Mystery Solution.sql
+++ Capstone Project/Mystery Solution.sql
@@ -2,12 +2,13 @@ USE idc_tech_nova;
 
 -- 1. Identify where and when the crime happened
 SELECT
-	room,
-    MIN(found_time) AS first_found_time,
-    GROUP_CONCAT(description) AS evidence_found
+	*
+	-- room,
+    -- found_time AS first_found_time,
+    -- description AS evidence_found
 FROM evidence
-WHERE room = 'CEO Office'
-GROUP BY room;
+WHERE room = 'CEO Office';
+-- GROUP BY room;
 
 -- 2. Analyze who accessed critical areas at the time
 SELECT 
@@ -19,7 +20,7 @@ SELECT
     kl.exit_time
 FROM keycard_logs kl
 JOIN employees e ON kl.employee_id = e.employee_id
-WHERE kl.entry_time BETWEEN '2025-10-15 20:50:00' AND '2025-10-15 21:10:00'
+WHERE kl.entry_time BETWEEN '2025-10-15 20:40:00' AND '2025-10-15 21:10:00'
 AND kl.room = 'CEO Office';
 
 -- 3. Cross-check alibis with actual logs
@@ -31,23 +32,35 @@ SELECT
     a.claim_time
 FROM employees e
 JOIN alibis a ON e.employee_id = a.employee_id
-WHERE a.claim_time BETWEEN '2025-10-15 20:50:00' AND '2025-10-15 21:10:00';
+WHERE a.claim_time BETWEEN '2025-10-15 20:50:00' AND '2025-10-15 21:10:00'
+AND claimed_location <> 'CEO Office';
 
+-- --------------------OR--------------------
 -- find employees whose claimed_location at ~20:50 doesn't match keycard location during same time
-WITH claims AS (
+WITH 
+claims AS (
   SELECT employee_id, claimed_location, claim_time
   FROM alibis
-  WHERE claim_time BETWEEN '2025-10-15 20:45' AND '2025-10-15 21:05'
+  WHERE claim_time BETWEEN '2025-10-15 20:40:00' AND '2025-10-15 21:10:00'
+),
+actual AS (
+	SELECT employee_id, room, entry_time, exit_time
+    FROM keycard_logs
+    WHERE entry_time BETWEEN '2025-10-15 20:40:00' AND '2025-10-15 21:10:00'
 )
-SELECT c.employee_id, emp.name,
-       c.claimed_location,
-       k.room AS actual_room,
-       k.entry_time, k.exit_time
+SELECT 
+	c.employee_id, 
+    e.name,
+	c.claimed_location,
+	a.room AS actual_room,
+	a.entry_time, 
+	a.exit_time
 FROM claims c
-LEFT JOIN keycard_logs k
-  ON c.employee_id = k.employee_id
-  AND (c.claim_time BETWEEN k.entry_time AND k.exit_time)
-JOIN employees emp ON c.employee_id = emp.employee_id
+LEFT JOIN actual a
+  ON c.employee_id = a.employee_id
+  AND (c.claim_time BETWEEN a.entry_time AND a.exit_time)
+JOIN employees e ON c.employee_id = e.employee_id	
+WHERE c.claimed_location <> a.room
 ORDER BY c.employee_id;
 
 
@@ -70,16 +83,21 @@ JOIN alibis a ON ev.room = a.claimed_location
 WHERE ev.found_time BETWEEN '2025-10-15 20:50:00' AND '2025-10-15 21:20:00';
 
 -- tie evidence in CEO Office to who was in that room earlier
-SELECT ev.evidence_id, ev.room, ev.description, ev.found_time,
-       k.employee_id, emp.name, k.entry_time, k.exit_time
+SELECT 
+	ev.evidence_id, 
+    ev.room, 
+    ev.description, 
+    ev.found_time,
+	k.employee_id, 
+    em.name, 
+    k.entry_time, 
+    k.exit_time
 FROM evidence ev
-LEFT JOIN keycard_logs k
-  ON ev.room = k.room 
+LEFT JOIN keycard_logs k ON ev.room = k.room 
   AND k.entry_time BETWEEN ev.found_time - INTERVAL 30 MINUTE AND ev.found_time
-JOIN employees emp ON k.employee_id = emp.employee_id
+JOIN employees em ON k.employee_id = em.employee_id
 WHERE ev.room = 'CEO Office'
 ORDER BY ev.found_time;
-
 
 -- 6. Combine all findings to identify the killer
 WITH
@@ -96,6 +114,7 @@ alibis_found AS (
 	SELECT *
 	FROM alibis
     WHERE claim_time BETWEEN '2025-10-15 20:40:00' AND '2025-10-15 21:20:00'
+    AND claimed_location <> 'CEO Office'
 ),
 sus_call AS (
 	SELECT * 
